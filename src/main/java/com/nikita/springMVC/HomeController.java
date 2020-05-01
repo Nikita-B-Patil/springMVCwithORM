d4dc9d524d3594249b356247f7f00cb54689ad67
Nikita Patil <47523083+Nikita-B-Patil@users.noreply.github.com>
2020-05-02 01:42:05 +0530

    updated

--- src/main/java/com/nikita/springMVC/HomeController.java
+++ src/main/java/com/nikita/springMVC/HomeController.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.nikita.springMVC;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -47,52 +47,4 @@ public class HomeController {
 		m.addAttribute("emp", dao.getEmployee(username));
 		return "showEmployee";
 	}
-	
-	
 }
-=======
-package com.nikita.springMVC;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import com.nikita.springMVC.dao.employeeDAO;
-
-@Controller
-public class HomeController {
-	
-	
-	@Autowired
-	employeeDAO dao;
-	
-	@RequestMapping("/")
-	public String home() {
-		return "index";
-	}
-	
-	@RequestMapping("addEmployee")
-	public String addEmployee(@ModelAttribute("employee") Employee employee) {		
-		dao.addEmployee(employee);
-		return "result";
-	}
-	
-	@GetMapping("getEmployees")
-	public String getEmployees(Model m) {
-		m.addAttribute("employees", dao.getEmployees());
-		return "showEmployees";
-	}
-	
-	@GetMapping("getEmployee")
-	public String getEmployee(@RequestParam String username, Model m) {
-		m.addAttribute("emp", dao.getEmployee(username));
-		return "showEmployee";
-	}
-	
-	
-}
->>>>>>> 7823c530b2d3a8c671d04c6a3664bb41255a878a
